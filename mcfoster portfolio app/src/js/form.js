dae3cf21dec2813506b276e4d9a35eb6d4406c2f
mcfoster007 <mariechantalfoster@gmail.com>
2024-11-04 22:16:12 -0500

    added to clear form and tried tjo connect to local storage

--- mcfoster portfolio app/src/js/form.js
+++ mcfoster portfolio app/src/js/form.js
@@ -1,31 +1,36 @@
-const name = document.getElementById('name');
-const email = document.getElementById('email');
-const message = document.getElementById('message');
-const button = document.querySelector('.button');
-const form = document.querySelector('form');
-const errorElement = document.getElementById('error');
 
-const handleFormSubmission = (event) => {
-    event.preventDefault(); 
+function test(){
+var name = document.getElementById('name').value;
+var email = document.getElementById('email').value;
+var message = document.getElementById('message').value;
+// var button = document.querySelector('.button').value;
+// var form = document.querySelector('form').value;
+// var errorElement = document.getElementById('error');
 
-    if (username.value && title.value && content.value) {
-        const contactFormData = {
-            name: name.value,
-            email: email.value,
-            message: message.value
-        };
-        const allContactData = JSON.parse (localStorage.getItem("userData")) || [];
-        allContactData.push(contactFormData)
+// const handleFormSubmission = (event) => {
+//     event.preventDefault(); 
+
+    // if (username.value && title.value && content.value) {
+    //     const contactFormData = {
+    //         name: name.value,
+    //         email: email.value,
+    //         message: message.value
+    //     };
+    //     const allContactData = JSON.parse (localStorage.getItem("userData")) || [];
+    //     allContactData.push(contactFormData)
         
-        localStorage.setItem("userData", JSON.stringify(allContactData));
-        window.location.href = 'blog.html';
-        name.value = '';
-        email.value = '';
-        message.value = '';
-    } else {
-        errorElement.textContent = "Error: Complete form."; 
+    //     localStorage.setItem("userData", JSON.stringify(allContactData));
+    //     window.location.href = 'contactform.html';
+    //     name.value = '';
+    //     email.value = '';
+    //     message.value = '';
+    // } else {
+    //     errorElement.textContent = "Error: Complete form."; 
       
-    }
+    // }
+    var name = localStorage.setItem("name", name);
+    var email = localStorage.setItem("email", email);
+    var message = localStorage.setItem("message", message);
 };
 
 // button.addEventListener('click', handleFormSubmission); 
